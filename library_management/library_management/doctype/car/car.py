492679a3687127f4db8f26e8ecd234563fa0d46e
alsubaei <alsubaeimanal@gmail.com>
2024-01-03 12:24:44 +0300

    add format validation for car

--- library_management/library_management/doctype/car/car.py
+++ library_management/library_management/doctype/car/car.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2023, Faris Ansari and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class Car(Document):
-	pass
+
+	def before_save(self):
+		self.validate()
+
+	def validate(self):
+		carplate = self.carplate
+		if len(carplate) != 8:
+			frappe.throw(_('The carplate must be in this fomrat abcd-123'))
+		if carplate[4] != '-' or  carplate[:4].isalpha() == False or carplate[5:].isnumeric() == False:
+			frappe.throw(_('The carplate must be in this format abcd-123'))
