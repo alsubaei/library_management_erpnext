3d481bed417bb2428e0d853773092677030856f9
alsubaei <alsubaeimanal@gmail.com>
2024-01-04 16:56:37 +0300

    add the bank loan with js function

---
+++ library_management/library_management/doctype/bank_loan/bank_loan.js
@@ -0,0 +1,29 @@
+// Copyright (c) 2024, Faris Ansari and contributors
+// For license information, please see license.txt
+frappe.ui.form.on("Bank Loan", {
+    // 	refresh(frm) {
+
+    // 	},
+    repayment_months: function (frm) {
+        calculate_monthly_repayment(frm);
+    },
+    load_amount: function (frm) {
+        calculate_monthly_repayment(frm);
+    },
+    interest: function (frm) {
+        calculate_monthly_repayment(frm);
+    }
+});
+function calculate_monthly_repayment(frm) {
+    if (frm.doc.load_amount && frm.doc.repayment_months) {
+        var monthly = frm.doc.load_amount / frm.doc.repayment_months;
+        var monthly = monthly + (monthly * frm.doc.interest / 100);
+        frm.doc.monthly_repayment = monthly;
+        refresh_field('monthly_repayment')
+    }
+    else {
+        frm.doc.monthly_repayment = 0;
+        refresh_field('monthly_repayment')
+    }
+}
+
