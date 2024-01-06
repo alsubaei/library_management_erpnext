f2964cfc477cb500dad498a054cbd9773092ba1d
alsubaei <alsubaeimanal@gmail.com>
2024-01-06 10:10:11 +0300

    add py code

--- library_management/library_management/doctype/bank_loan/bank_loan.py
+++ library_management/library_management/doctype/bank_loan/bank_loan.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2024, Faris Ansari and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.utils import add_to_date
 
 class BankLoan(Document):
-	pass
+	def on_update(self):
+		self.schedule =[]
+		date= self.repayment_date
+		amount= self.load_amount + self.load_amount * self.interest /100
+		for i in range(int(self.repayment_months)):
+			repayment = self.append('schedule',{})
+			repayment.payment_date = date
+			repayment.principal_amount= self.load_amount/self.repayment_months
+			repayment.interest_amount= repayment.principal_amount * self.interest/100
+			repayment.total_payment= repayment.principal_amount + repayment.interest_amount
+			repayment.balance_loan_amount = amount- repayment.total_payment
+			amount -= repayment.total_payment
+			date = add_to_date(date, months=1)
