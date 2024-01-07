dd10d6c5962f37fa32eca2a0e4c4de9064f9a975
alsubaei <alsubaeimanal@gmail.com>
2024-01-07 11:03:44 +0300

    add the journal ledger entry

--- library_management/library_management/doctype/bank_loan/bank_loan.js
+++ library_management/library_management/doctype/bank_loan/bank_loan.js
@@ -7,7 +7,7 @@ frappe.ui.form.on("Bank Loan", {
     repayment_months: function (frm) {
         calculate_monthly_repayment(frm);
     },
-    load_amount: function (frm) {
+    loan_amount: function (frm) {
         calculate_monthly_repayment(frm);
     },
     interest: function (frm) {
@@ -15,8 +15,8 @@ frappe.ui.form.on("Bank Loan", {
     }
 });
 function calculate_monthly_repayment(frm) {
-    if (frm.doc.load_amount && frm.doc.repayment_months) {
-        var monthly = frm.doc.load_amount / frm.doc.repayment_months;
+    if (frm.doc.loan_amount && frm.doc.repayment_months) {
+        var monthly = frm.doc.loan_amount / frm.doc.repayment_months;
         var monthly = monthly + monthly * frm.doc.interest / 100;
         frm.doc.monthly_repayment = monthly;
         refresh_field('monthly_repayment')
--- library_management/library_management/doctype/bank_loan/bank_loan.py
+++ library_management/library_management/doctype/bank_loan/bank_loan.py
@@ -3,19 +3,37 @@
 
 import frappe
 from frappe.model.document import Document
-from frappe.utils import add_to_date
+from frappe.utils import add_to_date,getdate,flt
 
 class BankLoan(Document):
+
+	def on_submit(self):
+		entry=frappe.new_doc("Journal Entry")
+		entry.posting_date=getdate()
+		#حساب الذي أودع فيه القروض
+		account=entry.append("accounts",{})
+		account.account = self.disbursement_account
+		account.debit= self.loan_amount
+		account.debit_in_account_currency=self.loan_amount
+		# حساب الذي أعطى الشركة القرض
+		account=entry.append("accounts",{})
+		account.account=self.loan_account
+		account.credit= self.loan_amount
+		account.credit_in_account_currency=self.loan_amount	
+		entry.insert()
+		entry.submit()
+
+
 	def on_update(self):
-		self.schedule =[]
-		date= self.repayment_date
-		amount= self.load_amount + self.load_amount * self.interest /100
+		self.schedule=[]
+		d=self.repayment_date
+		amount=self.loan_amount+self.loan_amount*self.interest/100
 		for i in range(int(self.repayment_months)):
-			repayment = self.append('schedule',{})
-			repayment.payment_date = date
-			repayment.principal_amount= self.load_amount/self.repayment_months
-			repayment.interest_amount= repayment.principal_amount * self.interest/100
-			repayment.total_payment= repayment.principal_amount + repayment.interest_amount
-			repayment.balance_loan_amount = amount- repayment.total_payment
-			amount -= repayment.total_payment
-			date = add_to_date(date, months=1)
+			repayment=self.append("schedule",{})
+			repayment.payment_date=d
+			repayment.principal_amount=self.loan_amount/self.repayment_months
+			repayment.interest_amount=repayment.principal_amount*self.interest/100
+			repayment.total_payment=repayment.principal_amount+repayment.interest_amount
+			repayment.balance_loan_amount=amount-repayment.total_payment
+			amount=amount-repayment.total_payment
+			d=add_to_date(d,months=1)
